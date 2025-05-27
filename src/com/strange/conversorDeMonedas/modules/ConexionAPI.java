d868da4c035500fea4d2f440819a9ad37c1a18d5
AbelIbarraSol <abelibarrasol18@gmail.com>
2025-05-27 02:26:03 -0500

    Direccion nueva

--- src/com/strange/conversorDeMonedas/modules/ConexionAPI.java
+++ src/com/strange/conversorDeMonedas/modules/ConexionAPI.java
@@ -9,25 +9,23 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class ConexionAPI {
-    public JsonObject conectarAPI(String moneda){
-        var direcccion = URI.create("https://v6.exchangerate-api.com/v6/ea53538a7170746b7cf35a66/latest/"+moneda);
+    public Double conectarAPI(String monedaBase, String monedaDestino){
+        var direcccion = URI.create("https://v6.exchangerate-api.com/v6/ea53538a7170746b7cf35a66/pair/"+monedaBase+"/"+monedaDestino);
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(direcccion)
                 .build();
 
         try {
-            HttpResponse<String> response = client
-                    .send(request, HttpResponse.BodyHandlers.ofString());
-
-            //Parsear el JSON obtenido.
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            //Parsear el response.body(texto plano) obtenido y convertirlo a Json otra vez.
             JsonObject json = JsonParser.parseString(response.body()).getAsJsonObject();
-            //Obtener clave especifica (sub objeto Json) del JSON completo.
-            JsonObject divisas = json.getAsJsonObject("conversion_rates");
+            //Obtener clave especifica () del JSON completo.
+            Double tasaDeconversion = json.get("conversion_rate").getAsDouble();
             //retornamos la lista de conversiones como un JsonObject
-            return divisas;
+            return tasaDeconversion;
         } catch (Exception e) {
-            throw new RuntimeException("Error en la conexión con la API");
+            throw new RuntimeException("Error en la conexión con la API"+e.getMessage());
         }
     }
 }
