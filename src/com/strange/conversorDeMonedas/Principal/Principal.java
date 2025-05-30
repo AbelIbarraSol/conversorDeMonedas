a4ca1b6bc5944f8c786a5f7ace6659f9218e309c
AbelIbarraSol <abelibarrasol18@gmail.com>
2025-05-30 15:30:36 -0500

    Agregar y generar conversiones en un reporte

--- src/com/strange/conversorDeMonedas/Principal/Principal.java
+++ src/com/strange/conversorDeMonedas/Principal/Principal.java
@@ -1,20 +1,24 @@
 package com.strange.conversorDeMonedas.Principal;
 
 import com.strange.conversorDeMonedas.modules.Conversor;
+import com.strange.conversorDeMonedas.modules.Reporte;
+
 import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Conversor conversor = new Conversor();
-
+        Reporte reporte = new Reporte();
         boolean opcion = true;
         while (opcion){
             Conversor.exhibirMenu();
             opcion = conversor.seleccionarOpcion(scanner.nextInt());
             if (opcion){
                 System.out.print("\nIngrese el monto que desea convertir: ");
-                System.out.println(conversor.conversion(scanner.nextDouble()));
+
+                reporte.almacenarReporte(conversor,scanner.nextDouble());
+                reporte.generarReporte();
             }
         }
         scanner.close();
